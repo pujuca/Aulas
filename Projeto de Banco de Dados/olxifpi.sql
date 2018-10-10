8706724b48797e43c070d7e14145d40b8cde93e0
pujuca <40705145+pujuca@users.noreply.github.com>
2018-10-10 19:02:13 -0300

    10/10/2018  19:02

--- Projeto de Banco de Dados/olxifpi.sql
+++ Projeto de Banco de Dados/olxifpi.sql
@@ -1,3 +1,4 @@
+-- 1
 create database olxifpi;
 
 use olxifpi;
@@ -8,11 +9,31 @@ create table estado(
 	primary key(UF)
 );
 
+-- 2 - povoar
+
+insert into estado values("PI","Piauí");
+insert into estado values("CE","Ceará");
+insert into estado values("MA","Maranhão");
+-- 3
+insert into estado values("PI","Piauí");
+-- Erro valor duplicado, pq já existe
+-- 4
+delete from estado
+where uf = "MA";
+
+
 create table categoria(
 	idCategoria int not null,
 	nome varchar(30),
 	primary key(idCategoria)
 );
+
+-- 5
+insert into categoria values(1,"Imoveis");
+insert into categoria values(2,"Veiculos");
+insert into categoria values(3,"Emprego");
+
+
 
 create table usuario(
 	idUsuario int not null,
@@ -20,6 +41,31 @@ create table usuario(
 	primary key(idUsuario)
 );
 
+-- 6
+alter table usuario
+add idade int;
+
+-- 7
+insert into usuario values(1,"Denival",40);
+insert into usuario values(2,"Marilia",30);
+insert into usuario values(3,"Jamilly",10);
+insert into usuario(idUsuario, nome) values(4,"Athanio");
+
+-- 8
+update usuario
+	set idade = 35
+where idUsuario = 4;
+
+-- 11
+alter table usuario
+add check(idade > 12);
+
+-- 12
+update usuario
+	set idade = 41
+where idUsuario = 1;
+
+-- 
 create table postagem(
 	numPostagem int not null,
 	idCategoria int not null,
@@ -29,3 +75,13 @@ create table postagem(
 	foreign key(idCategoria) references categoria(idCategoria),
 	foreign key(idUsuario) references usuario(idUsuario)
 );
+
+-- 9
+alter table postagem
+add estado varchar(2);
+-- 10
+alter table postagem
+add foreign key(estado) references estado(uf);
+
+
+
