d2f89ccee043cfe18a6ae38c450f53d2833109df
pujuca <40705145+pujuca@users.noreply.github.com>
2019-05-14 15:58:25 -0300

    14/05/2019 15:58

---
+++ Topicos Especiais - módulo 4/Manipulação de vetores.js
@@ -0,0 +1,25 @@
+//
+use preferencias
+//
+db.pessoa.insertMany([
+  {nome:"Denival", gosto:["musica","esporte"]},
+  {nome:"Antonio", gosto:["series","anime","gibi"]}  
+])
+
+//
+db.pessoa.find({})
+
+//
+db.pessoa.update(
+ {nome:"Antonio"},
+ {$push:{gosto:"balada"}}
+)
+
+db.pessoa.update(
+ {nome:"Denival"},
+ {$pull:{gosto:"musica"}} 
+) 
+ 
+ 
+
+
