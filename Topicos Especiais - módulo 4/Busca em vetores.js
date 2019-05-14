01c00972d2e6cfa4799899d61a7479feacc3e2d6
pujuca <40705145+pujuca@users.noreply.github.com>
2019-05-14 16:04:51 -0300

    14/05/2019 16:04

---
+++ Topicos Especiais - módulo 4/Busca em vetores.js
@@ -0,0 +1,13 @@
+//
+use preferencias
+
+//
+db.pessoa2.insertMany([
+  {nome:"Denival", contato:[{residencial:"33236005",celular:"998277464"}]},
+  {nome:"Antonio", contato:[{residencial:"33236003",celular:"998277465"}]}  
+])
+
+//
+db.pessoa2.find({"contato.celular":"998277464"})
+  
+
