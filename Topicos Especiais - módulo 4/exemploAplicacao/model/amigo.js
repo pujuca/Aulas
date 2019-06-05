c975c5c6534c09a9696e7f4abe57393a3b50c924
pujuca <40705145+pujuca@users.noreply.github.com>
2019-06-05 20:05:06 -0300

    05/06/2019 20:05

---
+++ Topicos Especiais - módulo 4/exemploAplicacao/model/amigo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var amigoSchema = new Schema({
+    nome:{
+        type:String,
+        require: true
+    },
+    apelido:{
+        type:String,
+        require: true
+    },
+    idade:{
+        type:Number,
+        require: true
+    }   
+})
+
+module.exports = mongoose.model('amigo', amigoSchema);
