073c3779c3926625ef402918d83ef331abb7ce7b
pujuca <40705145+pujuca@users.noreply.github.com>
2019-08-29 14:24:23 -0300

    29/08/2019 14:24

---
+++ Topicos Especiais - módulo 4/Exemplos/formulario.js
@@ -0,0 +1,12 @@
+
+
+function ola(){
+    let nome = document.querySelector("#nome").value;
+    //
+    let titulo = document.createElement("h1").appendChild(nome);
+    
+    //titulo.appendChild("nome");
+
+   // document.body.appendChild(titulo);
+}
+
